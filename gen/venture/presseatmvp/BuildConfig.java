17d8436818780fbdfa83ee726a3d03ff00918cd9
Itai Bez <itaibez96@gmail.com>
2013-08-19 21:52:21 +0300

    First Commit, all the project so far. Until JSON and read wrtie to file

---
+++ gen/venture/presseatmvp/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package venture.presseatmvp;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/venture/presseatmvp/RegisterActivity.java
@@ -0,0 +1,119 @@
+package venture.presseatmvp;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class RegisterActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.register);
+		if (fileList().length != 0) {
+			// Log.d("Pizza", "Files in dir");
+			EditText eName = (EditText) findViewById(R.id.registerName);
+			EditText ePhone = (EditText) findViewById(R.id.registerPhone);
+			EditText eAddress = (EditText) findViewById(R.id.registerAddress);
+
+			FileInputStream fis;
+			try {
+				fis = openFileInput("regInfo");
+				String json = readToString(fis);
+				JSONObject js = new JSONObject(json);
+				String jsonName = js.getString("name");
+				String jsonPhone = js.getString("phone");
+				String jsonAddress = js.getString("address");
+				eName.setText(jsonName);
+				ePhone.setText(jsonPhone);
+				eAddress.setText(jsonAddress);
+			} catch (FileNotFoundException e) {
+				Log.e("Pizza", "Read - Error File not found");
+				e.printStackTrace();
+			} catch (JSONException e) {
+				Log.e("Pizza", "Read - Json exception");
+				e.printStackTrace();
+			}
+			// Intent intent = new Intent(this.getApplicationContext(),
+			// HomeActivity.class);
+			// startActivity(intent);
+		} else {
+			Button b = (Button) findViewById(R.id.registerButton);
+			b.setOnClickListener(new OnClickListener() {
+
+				@Override
+				public void onClick(View v) {
+					EditText eName = (EditText) findViewById(R.id.registerName);
+					EditText ePhone = (EditText) findViewById(R.id.registerPhone);
+					EditText eAddress = (EditText) findViewById(R.id.registerAddress);
+					String name = eName.getText().toString();
+					String phone = ePhone.getText().toString();
+					String address = eAddress.getText().toString();
+					JSONObject jsonObject = new JSONObject();
+					try {
+						jsonObject.put("name", name);
+						jsonObject.put("phone", phone);
+						jsonObject.put("address", address);
+						Log.d("Pizza", jsonObject.toString());
+					} catch (Exception e) {
+						Log.e("Pizza", "Error in json");
+						e.printStackTrace();
+					}
+					try {
+						FileOutputStream fos = openFileOutput("regInfo",
+								Context.MODE_PRIVATE);
+						fos.write(jsonObject.toString().getBytes());
+						fos.close();
+					} catch (FileNotFoundException e1) {
+						Log.e("Pizza", "Write - Error File not found");
+						e1.printStackTrace();
+					} catch (IOException e) {
+						Log.e("Pizza", "Error in Write or Close, IO exception");
+						e.printStackTrace();
+					}
+					Intent intent = new Intent(getApplicationContext(),
+							HomeActivity.class);
+					startActivity(intent);
+				}
+
+			});
+			// Log.d("Pizza", "No Files in Dir");
+		}
+
+	}
+
+	public String readToString(FileInputStream fis) {
+		String s = "";
+		int cur;
+		byte[] bytes = new byte[0];
+		try {
+			cur = fis.read();
+			while (cur != -1) {
+				s += cur;
+				cur = fis.read();
+			}
+			bytes = new byte[s.length()];
+			for (int i = 0; i < s.length(); i++) {
+				bytes[i] = (byte) (s.charAt(i) - '0');
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return new String(bytes);
+	}
+}
