270c2bb156a85a4fe59e334c3347db3f851ed516
Itai Bez <itaibez96@gmail.com>
2013-08-20 01:01:08 +0300

    Memory Working. Button Functionable of first 2 layouts.

--- src/venture/presseatmvp/HomeActivity.java
+++ src/venture/presseatmvp/HomeActivity.java
@@ -1,7 +1,11 @@
 package venture.presseatmvp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class HomeActivity extends Activity {
 
@@ -9,7 +13,14 @@ public class HomeActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.home);
+		Button order = (Button) findViewById(R.id.homeOrder);
+		order.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(getApplicationContext(),
+						RegisterActivity.class);
+				startActivity(intent);
+			}
+		});
 	}
-	
-	
 }
--- src/venture/presseatmvp/RegisterActivity.java
+++ src/venture/presseatmvp/RegisterActivity.java
@@ -32,8 +32,19 @@ public class RegisterActivity extends Activity {
 
 			FileInputStream fis;
 			try {
+				// Read string from input
 				fis = openFileInput("regInfo");
-				String json = readToString(fis);
+				StringBuffer fileContent = new StringBuffer("");
+				byte[] buffer = new byte[1024];
+				while (fis.read(buffer) != -1) {
+					fileContent.append(new String(buffer));
+				}
+				String json = fileContent.toString();
+				json = json.substring(json.indexOf('{'),
+						json.lastIndexOf('}') + 1);
+				// X Read string from input
+
+				Log.d("Pizza", json);
 				JSONObject js = new JSONObject(json);
 				String jsonName = js.getString("name");
 				String jsonPhone = js.getString("phone");
@@ -47,73 +58,61 @@ public class RegisterActivity extends Activity {
 			} catch (JSONException e) {
 				Log.e("Pizza", "Read - Json exception");
 				e.printStackTrace();
+			} catch (IOException e) {
+				Log.e("Pizza", "Read - IOException");
+				e.printStackTrace();
 			}
 			// Intent intent = new Intent(this.getApplicationContext(),
 			// HomeActivity.class);
 			// startActivity(intent);
-		} else {
-			Button b = (Button) findViewById(R.id.registerButton);
-			b.setOnClickListener(new OnClickListener() {
-
-				@Override
-				public void onClick(View v) {
-					EditText eName = (EditText) findViewById(R.id.registerName);
-					EditText ePhone = (EditText) findViewById(R.id.registerPhone);
-					EditText eAddress = (EditText) findViewById(R.id.registerAddress);
-					String name = eName.getText().toString();
-					String phone = ePhone.getText().toString();
-					String address = eAddress.getText().toString();
-					JSONObject jsonObject = new JSONObject();
-					try {
-						jsonObject.put("name", name);
-						jsonObject.put("phone", phone);
-						jsonObject.put("address", address);
-						Log.d("Pizza", jsonObject.toString());
-					} catch (Exception e) {
-						Log.e("Pizza", "Error in json");
-						e.printStackTrace();
-					}
-					try {
-						FileOutputStream fos = openFileOutput("regInfo",
-								Context.MODE_PRIVATE);
-						fos.write(jsonObject.toString().getBytes());
-						fos.close();
-					} catch (FileNotFoundException e1) {
-						Log.e("Pizza", "Write - Error File not found");
-						e1.printStackTrace();
-					} catch (IOException e) {
-						Log.e("Pizza", "Error in Write or Close, IO exception");
-						e.printStackTrace();
-					}
-					Intent intent = new Intent(getApplicationContext(),
-							HomeActivity.class);
-					startActivity(intent);
+		}
+		Button b = (Button) findViewById(R.id.registerButton);
+		b.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				EditText eName = (EditText) findViewById(R.id.registerName);
+				EditText ePhone = (EditText) findViewById(R.id.registerPhone);
+				EditText eAddress = (EditText) findViewById(R.id.registerAddress);
+				String name = eName.getText().toString();
+				String phone = ePhone.getText().toString();
+				String address = eAddress.getText().toString();
+				JSONObject jsonObject = new JSONObject();
+				try {
+					jsonObject.put("name", name);
+					jsonObject.put("phone", phone);
+					jsonObject.put("address", address);
+					Log.d("Pizza", jsonObject.toString());
+				} catch (Exception e) {
+					Log.e("Pizza", "Error in json");
+					e.printStackTrace();
 				}
-
-			});
-			// Log.d("Pizza", "No Files in Dir");
-		}
+				try {
+					FileOutputStream fos = openFileOutput("regInfo",
+							Context.MODE_PRIVATE);
+					fos.write(jsonObject.toString().getBytes());
+					fos.close();
+				} catch (FileNotFoundException e1) {
+					Log.e("Pizza", "Write - Error File not found");
+					e1.printStackTrace();
+				} catch (IOException e) {
+					Log.e("Pizza", "Error in Write or Close, IO exception");
+					e.printStackTrace();
+				}
+				Intent intent = new Intent(getApplicationContext(),
+						HomeActivity.class);
+				startActivity(intent);
+			}
+		});
+		// Log.d("Pizza", "No Files in Dir");
 
 	}
-
-	public String readToString(FileInputStream fis) {
-		String s = "";
-		int cur;
-		byte[] bytes = new byte[0];
-		try {
-			cur = fis.read();
-			while (cur != -1) {
-				s += cur;
-				cur = fis.read();
-			}
-			bytes = new byte[s.length()];
-			for (int i = 0; i < s.length(); i++) {
-				bytes[i] = (byte) (s.charAt(i) - '0');
-			}
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return new String(bytes);
-	}
+	/*
+	 * public String readToString(FileInputStream fis) { String s = ""; int cur;
+	 * byte[] bytes = new byte[0]; try { cur = fis.read(readString.getBytes());
+	 * while (cur != -1) { s += cur + " "; cur = fis.read(); } bytes = new
+	 * byte[s.length()]; for (int i = 0; i < s.length(); i++) { byte b = (byte)
+	 * (s.charAt(i) - '0'); bytes[i] = b; Log.d("pizza", b + ""); } } catch
+	 * (IOException e) { // TODO Auto-generated catch block e.printStackTrace();
+	 * } return new String(bytes); }
+	 */
 }
